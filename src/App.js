f43073e24c57279f69094fdbc8a32b87c28a369d
Moemenakari <mouemenakkarie3@gmail.com>
2025-11-13 22:05:06 +0200

    feat: Successfully configured Tailwind CSS for styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
+// src/App.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-4xl font-extrabold text-center text-green-600 p-10">
     </div>
   );
 }
-
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // يجب أن يكون هذا المسار صحيحاً
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
